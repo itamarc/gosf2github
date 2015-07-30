6c03336fdbe242bd17fbea6e511e9ce23ee5063e
raffazizzi <raffaeleviglianti@gmail.com>
2015-07-30 16:25:18 -0400

    node script to parse sf users from export data and check for their usernames on github

---
+++ js/get_names.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+var https = require('https');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+var bugs = JSON.parse(fs.readFileSync('data/bugs.json', 'utf8'));
+var frs = JSON.parse(fs.readFileSync('data/feature-requests.json', 'utf8'));
+
+// Eample structure:
+// { "reported" : { 
+// 	"with_ids" : {"id" : ['name1', 'name2']},
+// 	"names_only": ["name1", "name2"]
+//   },
+//   "assigned" : { ... }
+// }
+names = {}
+
+find_names_by_role = function (tickets, role) {
+	role_id_field = role + "_id";
+	role_field = role;
+	names[role] = {"with_ids":{}, "names_only":[]}
+	tickets.forEach(function(ticket){
+		name_id = ticket[role_id_field];
+		name = ticket[role_field];
+		pers = names[role]["with_ids"][name_id]
+		if (pers != null) {
+			if (pers.indexOf(name) == -1) {
+				pers.push(name);
+			}
+		}
+		else {
+			names[role]["with_ids"][name_id] = [ticket[role_field]];	
+		}
+    // Add to names only list
+    if (names[role]["names_only"].indexOf(name) == -1) {
+      names[role]["names_only"].push(name);
+    }
+	});
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+// Reporters
+find_names_by_role(bugs["tickets"], "reported_by");
+find_names_by_role(frs["tickets"], "reported_by");
+
+// Assignees
+find_names_by_role(bugs["tickets"], "assigned_to");
+find_names_by_role(frs["tickets"], "assigned_to");
+
+fs.writeFile("data/tei-names.json", JSON.stringify(names, null, 2));
+
+// Merged list of names
+all_names = {}
+roles = Object.keys(names)
+roles.forEach(function(role, r_i) {
+  names[role]["names_only"].forEach(function(n, n_i){
+    if (n in all_names == false) {
+      
+      // Check with GitHub if the user exists
+
+      var options = {
+          host: 'github.com',
+          path: '/' + n,
+          method: 'GET',
+          headers: {'user-agent': 'node.js'}
+      };
+
+      found_on_github = false;
+
+      https.get(options, function(res){
+        res.on('data', function(d) {
+          if (res.statusCode == 200) {
+            found_on_github = true;
+          }
+        });
+        res.on('end', function(){
+          all_names[n] = found_on_github ? n : "";
+          if (n_i+1 == names[role]["names_only"].length && r_i+1 == roles.length) {
+            eventEmitter.emit("doneGH");
+          }
+        });
+      }).end();
+    }
+ });
+});
+
+eventEmitter.on('doneGH', function() {
+  console.log('done');
+  fs.writeFile("data/tei-sf-users.json", JSON.stringify(all_names, null, 2));
+});
